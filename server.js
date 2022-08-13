48c7b08ecd23e879818e98edc63ead2dfc0e0638
Tanya <REINACREATES1@GMAIL.com>
2022-08-13 16:48:54 -0400

    changes

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.get('/api/:illnessName', (request, response) => {
 })
 
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
   console.log(`The server is running on port ${PORT})! You better go catch it!`)
 })
